30565d8ef502e5f64c7614c0407bb68ee043d13c
wanglf1207 <wanglf1207@163.com>
2021-03-17 01:01:26 +0800

    http

---
+++ netty4-demo/src/main/java/http/ClientHandler.java
@@ -0,0 +1,50 @@
+package http;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+import java.net.URI;
+
+/**
+ * 客户端处理器
+ */
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        FullHttpResponse response = (FullHttpResponse) msg;
+
+        ByteBuf content = response.content();
+        HttpHeaders headers = response.headers();
+
+        System.out.println("content:" + System.getProperty("line.separator") + content.toString(CharsetUtil.UTF_8));
+        System.out.println("headers:" + System.getProperty("line.separator") + headers.toString());
+    }
+
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        URI url = new URI("");
+        String meg = "<test>ssss</test>";
+
+        //配置HttpRequest的请求数据和一些配置信息
+        FullHttpRequest request = new DefaultFullHttpRequest(
+                HttpVersion.HTTP_1_0, HttpMethod.GET, url.toASCIIString(), Unpooled.wrappedBuffer(meg.getBytes("UTF-8")));
+
+        request.headers()
+                .set(HttpHeaderNames.CONTENT_TYPE, "text/plain;charset=UTF-8")
+                //开启长连接
+                .set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE)
+                //设置传递请求内容的长度
+                .set(HttpHeaderNames.CONTENT_LENGTH, request.content().readableBytes());
+
+        //发送数据
+        ctx.writeAndFlush(request);
+    }
+}
